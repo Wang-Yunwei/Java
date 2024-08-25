b23462638c482bea9e4a19adeb229d867c3bc72f
访卉念微 <wangyunwei_@outlook.com>
2024-08-25 16:40:49 +0800

    2024年8月25日 16点40分

--- Java_17/SocketTransferServer/src/main/java/com/mdsd/cloud/controller/tyjw/service/impl/AuthServiceImpl.java
+++ Java_17/SocketTransferServer/src/main/java/com/mdsd/cloud/controller/tyjw/service/impl/AuthServiceImpl.java
@@ -1,28 +1,22 @@
 package com.mdsd.cloud.controller.tyjw.service.impl;
 
-import com.mdsd.cloud.controller.tyjw.common.AbstractShareMethod;
 import com.mdsd.cloud.controller.tyjw.dto.AuthSingleton;
-import com.mdsd.cloud.controller.tyjw.dto.GetCloudBoxListOup;
 import com.mdsd.cloud.controller.tyjw.dto.GetTokenInp;
 import com.mdsd.cloud.controller.tyjw.dto.GetTokenOup;
 import com.mdsd.cloud.controller.tyjw.service.AuthService;
-import com.mdsd.cloud.enums.StateEnum;
 import com.mdsd.cloud.feign.EApiFeign;
 import com.mdsd.cloud.response.BusinessException;
 import com.mdsd.cloud.response.ResponseTy;
 import com.mdsd.cloud.utils.MD5HashGenerator;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 /**
  * @author WangYunwei [2024-07-12]
  */
 @Slf4j
 @Service
-public class AuthServiceImpl extends AbstractShareMethod implements AuthService {
+public class AuthServiceImpl implements AuthService {
 
     private final EApiFeign feign;
 
@@ -48,13 +42,14 @@ public class AuthServiceImpl extends AbstractShareMethod implements AuthService 
         log.info("获取 AccessToken");
         String str = param.getAccessKeyId() + param.getAccessKeySecret() + param.getTimeStamp();
         param.setEncryptStr(MD5HashGenerator.generateMD5(str));
-        return handleAuth(() ->{
-            ResponseTy<GetTokenOup> result = feign.getToken(param);
-            GetTokenOup getTokenOup = processResult(result);
-            AuthSingleton.getInstance().setCompanyId(getTokenOup.getCompanyId());
-            AuthSingleton.getInstance().setAccessToken(getTokenOup.getAccessToken());
-            return getTokenOup;
-        });
+        ResponseTy<GetTokenOup> result = feign.getToken(param);
+        if(0== result.getState()){
+            AuthSingleton.getInstance().setCompanyId(result.getContent().getCompanyId());
+            AuthSingleton.getInstance().setAccessToken(result.getContent().getAccessToken());
+            return result.getContent();
+        }else{
+            throw new BusinessException(String.valueOf(result.getState()), result.getMessage());
+        }
     }
 }
 
