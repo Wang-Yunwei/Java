99cf009d62b7dff7ee9031ce483406c7d85914b2
Wang Yunwei <wangyunwei_@outlook.com>
2024-09-23 09:31:53 +0800

    add UDP Client

--- Java_17/SocketTransferServer/src/main/java/com/mdsd/cloud/utils/UdpSocket.java
+++ Java_17/SocketTransferServer/src/main/java/com/mdsd/cloud/utils/UdpSocket.java
@@ -10,6 +10,8 @@ import io.netty.channel.socket.DatagramPacket;
 import io.netty.channel.socket.nio.NioDatagramChannel;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ArrayBlockingQueue;
@@ -17,29 +19,43 @@ import java.util.concurrent.ArrayBlockingQueue;
 /**
  * @author WangYunwei [2024-09-14]
  */
-
+@Slf4j
+@Component
 public class UdpSocket {
+
+    @Value("${env.port.sts.udp}")
+    private int port;
+
     NioEventLoopGroup eventExecutors = new NioEventLoopGroup();
 
+    @PostConstruct
     public void createUdpSocket() {
         Bootstrap bootstrap = new Bootstrap();
         bootstrap.group(eventExecutors)
                 .channel(NioDatagramChannel.class)
-                .option(ChannelOption.SO_SNDBUF,4194304) // 4MB 发送缓冲区
+                .option(ChannelOption.SO_SNDBUF, 4194304) // 4MB 发送缓冲区
                 .option(ChannelOption.SO_RCVBUF, 4194304) // 4MB 接收缓冲区
                 .option(ChannelOption.SO_REUSEADDR, true) // 启用地址重用
                 .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT) // // 使用池化缓冲区分配器
                 .handler(new SimpleChannelInboundHandler<DatagramPacket>(false) {
                     @Override
                     protected void channelRead0(ChannelHandlerContext ctx, DatagramPacket pak) throws Exception {
-                        pak.content().readableBytes();
-                        ArrayBlockingQueue<DatagramPacket> objects = new ArrayBlockingQueue<>(1024);
-                        objects.offer(pak);
+//                        pak.content().readableBytes();
+//                        ArrayBlockingQueue<DatagramPacket> objects = new ArrayBlockingQueue<>(1024);
+//                        objects.offer(pak);
+                        ctx.writeAndFlush(pak);
+
                     }
                 });
-
+        try {
+            log.info("<<< 启动UDP, 端口: {}", port);
+            bootstrap.bind(port).sync();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    @PreDestroy
     public void destroy() {
         eventExecutors.shutdownGracefully();
     }
